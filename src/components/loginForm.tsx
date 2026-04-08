827b79d591c660827b4e05743761fe8e938ae2f3
Guilherme Alves de Lima <114428628+Guilherme-lima545@users.noreply.github.com>
2026-04-08 16:35:50 -0300

    Update loginForm.tsx

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -83,10 +83,10 @@ export default function LoginForm() {
         Putz! Esqueci minha Senha
       </Link>
       <div className={styles.cadastro}>
-        <h2 className={styles.subtitle}> Cadrastre-se Aqui.</h2>
+        <h2 className={styles.subtitle}> Cadastre - se Aqui.</h2>
         <p> Novo por aqui? </p>
-        <Link href="/login/cadrastro" className={stylesbt.button}>
-          Cadrastro
+        <Link href="/login/cadastro" className={stylesbt.button}>
+          Cadastro
         </Link>
       </div>
     </section>
